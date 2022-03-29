f24126e71578f70c16913afc90d1df5ef64571e7
401 <401@academicos.cesde.edu.co>
2022-03-29 11:30:52 -0500

    se genera token automaticamente

--- js/GenerarToken.js
+++ js/GenerarToken.js
@@ -1,1 +1,30 @@
-export let token="Bearer BQAf-vOUuM1Q1hP6kVSvxM_uSrkvu5op5lrGaUkF3sxCkg7lJku1lxRmN5LyFoPBBR1hiThP711LdxhlcfDeTI-ncaTLfj0FsDMxXMzv9oPeyQXLrDfhOIr0MvoBgy1Dff21uT7yPK0GYdwDokRLfv2koJTTG0A"
+export async function generartoken(){
+    let URI="https://accounts.spotify.com/api/token"
+
+    let client_id="client_id=b2a10e65a42f41b2b127cca70db0ed3e"
+    let client_secret="client_secret=68c5c4194f0849e8af72aa11713324e6"
+    let grant_type="grant_type=client_credentials"
+
+    let parametros={
+        method:"POST",
+        headers:{"Content-Type":"application/x-www-form-urlencoded"},
+        body:`${client_id}&${client_secret}&${grant_type}`
+    }
+
+    let respuesta = await fetch(URI,parametros)
+    let respuestafinal=await respuesta.json()
+    let token=(`${respuestafinal.token_type} ${respuestafinal.access_token}`)
+    return(token)
+    
+    
+   /* fetch(URI,parametros)
+    .then(function(respuesta){
+        return(respuesta.json())
+    })
+    .then(function(respuesta){
+        console.log(respuesta)
+    })
+    .catch(function(respuesta){
+        console.log(respuesta)
+    })*/
+}
--- js/controlador.js
+++ js/controlador.js
@@ -1,7 +1,7 @@
 import { nombre } from "./GeneradorNombre.js";
 import { estatura } from "./GeneradorEstatura.js";
 import {universidad} from "./Universidad.js";
-import {token} from "./GenerarToken.js";
+import {generartoken} from "./GenerarToken.js";
 import {generarURI} from "./GeneradorURI.js"
 import {consumirAPI} from "./servicio.js"
 
@@ -13,6 +13,7 @@ boton.addEventListener("click",function(evento){
     let URI=generarURI(frutaSeleccionada)
 
     async function activarAPI(){
+        let token=await generartoken()
         let canciones=await consumirAPI(URI,token)
         console.log(canciones)
     }
